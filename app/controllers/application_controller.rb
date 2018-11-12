8e20317fbe07ca254c7938e3304957143c9985f3
Rwen <rwen@dowelltech.cn>
2018-11-12 16:21:34 +0800

    temp disable weixin auth
    temp disable weixin auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :get_wechat_sns, if: :is_wechat_browser?
+    # before_action :get_wechat_sns, if: :is_wechat_browser?
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
